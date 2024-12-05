113f67317950e30a832f8305a216f69830a4fca8
Strocs <strocsdev@gmail.com>
2024-12-04 22:51:00 -0300

    update nvim dependencies

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,6 +11,16 @@ return {
     end,
   },
   {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "sonokai",
