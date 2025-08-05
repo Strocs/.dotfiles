98c4c90cdf300df912926bedb8eb1be9b690251f
Strocs <strocsdev@gmail.com>
2025-08-04 22:13:36 -0400

    feat(zsh): Start wezterm-config-sync-daemon

--- .config/.zsh/general.zsh
+++ .config/.zsh/general.zsh
@@ -25,3 +25,10 @@ export EDITOR='nvim'
 # COMPLETION_WAITING_DOTS="true"
 # DISABLE_UNTRACKED_FILES_DIRTY="true"
 # HIST_STAMPS="mm/dd/yyyy"
+
+
+# Start WezTerm config sync daemon if not already running
+if ! pgrep -f "wezterm-config-sync-daemon.sh" > /dev/null; then
+  nohup /home/strocsdev/.dotfiles/wezterm/wezterm-config-sync-daemon.sh &
+fi
+
