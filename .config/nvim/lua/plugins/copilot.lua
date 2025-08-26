388017314c50848f44868e82dbb84df531a0f8ef
Strocs <strocsdev@gmail.com>
2025-08-26 14:33:33 -0400

    refactor(nvim): update Copilot plugin configuration

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,26 +2,6 @@ return {
   "zbirenbaum/copilot.lua",
   optional = true,
   opts = function()
-    require("copilot").setup({
-      panel = {
-        enabled = true, -- Disable the Copilot panel
-      },
-      suggestion = {
-        enabled = true, -- Enable Copilot suggestions
-        auto_trigger = true, -- Automatically trigger suggestions
-        debounce = 75, -- Debounce time for suggestions
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false, -- Disable Copilot for all other filetypes
-      },
-    })
+    require("copilot.api").status = require("copilot.status")
   end,
 }
