c59b93f0536eafbd19663cf7d340d29024dce979
Strocs <strocsdev@gmail.com>
2026-01-30 15:41:22 -0300

    chore(nvim): remove deprecated backup config
    Remove nvim_bak directory containing obsolete backup configuration

--- .config/nvim_bak/lua/config/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.options")
-require("config.remap")
-require("config.lazy")
-
--- Netrw configuration for file explorer
-vim.g.netrw_browse_split = 0
--- vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
