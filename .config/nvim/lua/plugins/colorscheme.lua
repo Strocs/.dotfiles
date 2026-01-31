fd9b45178a34a2320e81a89c15bd361f00925001
Strocs <strocsdev@gmail.com>
2026-01-31 16:58:13 -0300

    feat(plugins): add monokai-v2.nvim colorscheme option

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,9 +2,6 @@ local theme = 'tokyonight'
 
 return {
   {
-    -- Plugin: sonokai
-    -- URL: https://github.com/sainnhe/sonokai
-    -- Description: High Contrast & Vivid Color Scheme based on Monokai Pro
     'sainnhe/sonokai',
     lazy = false,
     priority = 1000,
@@ -14,6 +11,9 @@ return {
       vim.g.sonokai_better_performance = 1
       vim.g.sonokai_transparent_background = 2
       vim.g.sonokai_diagnostic_virtual_text = 'colored'
+
+      if theme ~= 'sonokai' then return end
+      vim.cmd.colorscheme 'sonokai'
     end,
   },
   {
@@ -28,7 +28,20 @@ return {
       },
     },
     config = function()
-      if theme == 'tokyonight' then vim.cmd.colorscheme 'tokyonight' end
+      if theme ~= 'tokyonight' then return end
+      vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+  {
+    'khoido2003/monokai-v2.nvim',
+    priority = 1000,
+    config = function()
+      if theme ~= 'monokai' then return end
+      require('monokai-v2').setup {
+        transparent_background = true,
+        filter = 'spectrum',
+      }
+      vim.cmd 'colorscheme monokai-v2'
     end,
   },
 }
