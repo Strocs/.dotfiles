eb4ae8640831627242622c50f210a50981114049
Strocs <strocsdev@gmail.com>
2026-01-31 14:38:01 -0300

    refactor(treesitter): migrate to main branch and reconfigure
    - Update nvim-treesitter to main branch
    - Change branch from 'master' to 'main'
    - Add lazy = false and build = ':TSUpdate'
    - Expand ensure_installed parsers: html, json, jsonc, xml, yaml, astro, go, python, diff, markdown, markdown_inline, printf, query, regex, toml
    - Enable indent, highlight, and folds modules
    - Add nvim-treesitter-context dependency
    - Add nvim-ts-autotag plugin
    - Remove deprecated templ parser configuration

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,89 +1,75 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all"
-        ensure_installed = {
-          "vimdoc",
-          "javascript",
-          "typescript",
-          "c",
-          "lua",
-          "jsdoc",
-          "bash",
-          "go",
-        },
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
+    build = ':TSUpdate',
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+    dependencies = { 'nvim-treesitter/nvim-treesitter-context' },
+    opts_extend = { 'ensure_installed' },
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-        auto_install = true,
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'printf',
+        'python',
+        'query',
+        'regex',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+        'astro',
+        'go',
+      },
+      sync_install = false,
 
-        indent = {
-          enable = true,
-        },
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
 
-        highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-          disable = function(lang, buf)
-            if lang == "html" then
-              print("disabled")
-              return true
+      indent = { enable = true },
+      highlight = { enable = true },
+      folds = { enable = true },
+    },
+
+    -- Forzar inicialización de Treesitter highlight al abrir archivos
+    -- Esto es necesario porque Treesitter highlight a veces no se inicia automáticamente
+    -- Ver issues: nvim-treesitter#3767, nvim-treesitter#5750, nvim-treesitter#5207
+    config = function(_, opts)
+      local TS = require 'nvim-treesitter'
+      TS.setup(opts)
+
+      vim.api.nvim_create_autocmd('BufReadPost', {
+        group = vim.api.nvim_create_augroup('TreesitterInit', { clear = true }),
+        callback = function()
+          vim.defer_fn(function()
+            local ts_enabled, ts_status = pcall(vim.treesitter.start, vim.api.nvim_get_current_buf())
+            if not ts_enabled then
+              print('Treesitter highlight no pudo inicializarse automáticamente')
             end
-
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              vim.notify("File larger than 100KB treesitter disabled for performance", vim.log.levels.WARN, { title = "Treesitter" })
-              return true
-            end
-          end,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = { "markdown" },
-        },
+          end, 10)
+        end,
       })
-
-      local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      treesitter_parser_config.templ = {
-        install_info = {
-          url = "https://github.com/vrischmann/tree-sitter-templ.git",
-          files = { "src/parser.c", "src/scanner.c" },
-          branch = "master",
-        },
-      }
-
-      vim.treesitter.language.register("templ", "templ")
     end,
   },
 
   {
-    "nvim-treesitter/nvim-treesitter-context",
-    after = "nvim-treesitter",
-    config = function()
-      require("treesitter-context").setup({
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        multiwindow = false, -- Enable multiwindow support.
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      })
-    end,
+    'windwp/nvim-ts-autotag',
+    opts = {},
   },
 }
